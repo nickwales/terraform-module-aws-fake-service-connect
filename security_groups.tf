d1dbda3f2160421d65447e5495c85bcfde634c89
Nick Wales <588472+nickwales@users.noreply.github.com>
2024-08-30 17:18:50 -0500

    First commit

---
+++ security_groups.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "sg" {
+  name_prefix = "${var.name}-${var.consul_datacenter}-frontend"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "Instance ingress"
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_security_group" "lb" {
+  name_prefix = "${var.name}-${var.consul_datacenter}-${var.name}"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
